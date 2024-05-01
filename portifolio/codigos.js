0e0d5961e54774216fe8b879a6ccc822a891b48f
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-05-01 15:19:23 -0300

    Botão de menu com animação

--- portifolio/codigos.js
+++ portifolio/codigos.js
@@ -1,2 +1,20 @@
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+const navbarList = document.querySelector('.navbar-list');
 
+menuButton.addEventListener('click', aparecerMenu);
 
+function aparecerMenu() {
+    menu.classList.add('menu');
+    navbarList.classList.add('menu');
+
+    const menuVisible = menu.style.right === '0px';
+    if (menuVisible) {
+        menu.style.right = '-200px';
+        navbarList.style.marginRight = '0';
+    } else {
+        menu.style.right = '0px';
+        navbarList.style.marginRight = '200px';
+    }
+    
+}
