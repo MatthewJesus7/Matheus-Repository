08053641ac6e2e440745bffd6321a135696bd76e
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-05-04 12:58:07 -0300

    alterações no Menu, e em toda a paleta de cores

--- portifolio/codigos.js
+++ portifolio/codigos.js
@@ -1,3 +1,10 @@
+// Cores
+const corDestaqueClaro = getComputedStyle(document.documentElement).getPropertyValue('--corClara');
+const corDestaqueT = getComputedStyle(document.documentElement).getPropertyValue('--corDestaqueT');
+const corDestaqueP = getComputedStyle(document.documentElement).getPropertyValue('--corDestaqueP');
+const corDestaqueEscuro = getComputedStyle(document.documentElement).getPropertyValue('--corDestaqueEscuro');
+
+// Botão fazer menu aparecer
 const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
 const navbarList = document.querySelector('.navbar-list');
@@ -12,9 +19,13 @@ function aparecerMenu() {
     if (menuVisible) {
         menu.style.right = '-200px';
         navbarList.style.marginRight = '0';
+        menu.style.backgroundColor = ''
+        menuButton.style.backgroundColor = ''
     } else {
         menu.style.right = '0px';
         navbarList.style.marginRight = '200px';
+        menu.style.backgroundColor = corDestaqueEscuro
+        menuButton.style.backgroundColor = corDestaqueEscuro
     }
     
 }
